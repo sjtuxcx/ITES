a8f61a7774c274b83955f7eba66259550df6cfdc
sjtuxcx <1287280377@qq.com>
2021-07-06 16:48:28 +0800

    update

--- data/convert_cdf_to_mat.m
+++ data/convert_cdf_to_mat.m
@@ -1,12 +1,4 @@
-% Copyright (c) 2018-present, Facebook, Inc.
-% All rights reserved.
-%
-% This source code is licensed under the license found in the
-% LICENSE file in the root directory of this source tree.
-%
 
-% Extract "Poses_D3_Positions_S*.tgz" to the "pose" directory
-% and run this script to convert all .cdf files to .mat
 
 pose_directory = 'pose';
 dirs = dir(strcat(pose_directory, '/*/MyPoseFeatures/D3_Positions/*.cdf'));
